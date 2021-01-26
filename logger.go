121f42f49b3b1c52661f521afb3dabdbf03070cd
code4trade <luccazanchitrader@gmail.com>
2021-01-25 22:30:25 -0300

    Add the logger extension

---
+++ logger.go
@@ -0,0 +1,50 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/evalphobia/logrus_sentry"
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+// F is a map used to log custom fields.
+type F map[string]interface{}
+
+//Init initializes the logger to be used in any package.
+func init() {
+	logger = logrus.New()
+
+	dsn, exists := os.LookupEnv("SENTRY_DSN")
+
+	if exists {
+		hook, err := logrus_sentry.NewSentryHook(dsn, []logrus.Level{
+			logrus.PanicLevel,
+			logrus.FatalLevel,
+			logrus.ErrorLevel,
+		})
+		if err != nil {
+			logger.Warnf("It was not possible to enable Sentry handler: %w", err)
+		}
+
+		hook.StacktraceConfiguration.Enable = true
+
+		logger.Hooks.Add(hook)
+	}
+}
+
+// WithFields log custom fields.
+func WithFields(fields map[string]interface{}) *logrus.Entry {
+	return logger.WithFields(fields)
+}
+
+// Info log the args as informative data.
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+// Error log the args as an error.
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
